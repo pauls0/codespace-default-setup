5ba95e04dd98d888e4f66f7f4e43b1a07fb3ba56
Paul <69425241+pauls0@users.noreply.github.com>
2023-02-09 01:51:11 +0000

    .devcontainer

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,7 +3,7 @@ echo "Running install script...";
 sudo apt update -y;
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+echo 'y' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/vscode/.profile;
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.profile;
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)";
